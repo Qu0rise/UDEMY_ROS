2ca73804c8e0cf4a946512c1504d5a8a4f1dd44e
quorise <daison9200reply@gmail.com>
2022-09-04 16:26:35 +0900

    first commit

---
+++ build/my_robot_msgs/CMakeFiles/my_robot_msgs_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/ros/catkin_ws/devel/lib/python3/dist-packages/my_robot_msgs/msg/_HardwareStatus.py"
+  "/home/ros/catkin_ws/devel/lib/python3/dist-packages/my_robot_msgs/msg/__init__.py"
+  "/home/ros/catkin_ws/devel/lib/python3/dist-packages/my_robot_msgs/srv/_ComputeDiskArea.py"
+  "/home/ros/catkin_ws/devel/lib/python3/dist-packages/my_robot_msgs/srv/_SetLed.py"
+  "/home/ros/catkin_ws/devel/lib/python3/dist-packages/my_robot_msgs/srv/__init__.py"
+  "CMakeFiles/my_robot_msgs_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/my_robot_msgs_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
