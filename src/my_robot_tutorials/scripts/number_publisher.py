f7c76e4bd1915027d004b9c3db48a656b7c4b6e9
quorise <daison9200reply@gmail.com>
2022-09-06 10:43:41 +0900

    add

--- src/my_robot_tutorials/scripts/number_publisher.py
+++ src/my_robot_tutorials/scripts/number_publisher.py
@@ -6,10 +6,16 @@ from std_msgs.msg import Int64
 if __name__ == '__main__':
     rospy.init_node("number_publisher",anonymous=True)
     pub = rospy.Publisher("/number", Int64, queue_size=10)
-    rate = rospy.Rate(1)
+
+
+    publish_frequency = rospy.get_param("/number_publish_frequency")
+    rate = rospy.Rate(publish_frequency)
+
+    number = rospy.get_param("/number_to_publish")
+
     while not rospy.is_shutdown():
         msg = Int64()
-        msg.data = 2
+        msg.data = number
         pub.publish(msg)
         rate.sleep()
 
